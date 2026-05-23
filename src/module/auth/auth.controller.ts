eac70c6f41afbfd4ca2cb1ab81ebe5ad660acdac
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-05-23 10:12:48 +0600

    fix: resolve login issues

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,21 +1,29 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { authService } from "./auth.service.js";
 
-const loginUser = async (req: Request, res: Response) => {
-    try {
-        const result = await authService.userLoginIntoDb(req.body)
-        res.status(200).json({
-            success: true,
-            data: result
-        })
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
+const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
 
-        })
-    }
-}
+  try {
+
+    const result = await authService.userLoginIntoDb(req.body);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+
+  } catch (error) {
+
+    next(error);
+
+  }
+};
 
 export const authController = {
-    loginUser
-}
+  loginUser,
+};
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { pool } from "../../db/index.js";
 import console from "node:console";
+import bcrypt from "bcryptjs";
 
 const userLoginIntoDb = async (payload:
     {
@@ -8,21 +9,24 @@ const userLoginIntoDb = async (payload:
         password: string
     }) => {
 
-        const {email, password} = payload
+    const { email, password } = payload
 
-        const userData = await pool.query(`
-           SELECT * FROM users WHERE email = $1,
+    const userData = await pool.query(`
+           SELECT * FROM users WHERE email = $1
         `, [email])
-        
-        if(userData.rows.length === 0){
-              throw new Error("Invalid Credentials")
-        }
-        const user = userData.rows[0]
-        console.log(user.password)
+
+    if (userData.rows.length === 0) {
+        throw new Error("Invalid Credentials")
+    }
+    const user = userData.rows[0]
+    const isPassWord = await bcrypt.compare(password, user.password)
+    if (!isPassWord) {
+        throw new Error("Invalid Credentials")
+    }
 
 }
 
 export const authService = {
-   
-   userLoginIntoDb
+
+    userLoginIntoDb
 };
