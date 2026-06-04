108d411e6c1d0b27a20a629e228ff091c75b1554
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-06-04 10:29:02 +0600

    fix: resolve the issues of signin in vercel

--- src/db/index.ts
+++ src/db/index.ts
@@ -20,7 +20,7 @@ export const initDB = async () => {
         name VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password TEXT NOT NULL,
-        role VARCHAR(20) DEFAULT 'contributor'
+        role  VARCHAR(20) DEFAULT 'contributor'
         CHECK(role IN ('contributor', 'maintainer')),
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
@@ -30,7 +30,7 @@ export const initDB = async () => {
         CREATE TABLE IF NOT EXISTS issues(
         id SERIAL PRIMARY KEY,
         title VARCHAR(150) NOT NULL,
-        description TEXT NOT NULL,
+        description TEXT NOT NULL CHECK(LENGTH(description) >= 20),
         type VARCHAR(30) NOT NULL CHECK(type IN ('bug', 'feature_request')),
         status VARCHAR(30) DEFAULT 'open' CHECK(status IN ('open', 'in_progress', 'resolved')),
         reporter_id INT NOT NULL,
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -11,7 +11,7 @@ const createUserIntoDB = async (payload: IUser) => {
           users(name, email, password, role) 
           VALUES($1, $2, $3, $4)
           RETURNING *` ,
-        [name, email, hashPassword, role]
+        [name, email, hashPassword, role || 'contributor']
 
     )
     delete result.rows[0].password
